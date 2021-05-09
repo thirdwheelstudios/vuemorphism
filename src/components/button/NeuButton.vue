9a0e5d733453f4803747189359a156303d5f0615
Phil Crossland <phil@thirdwheelstudios.com>
2021-05-09 22:29:23 +0100

    Initial button styling

--- src/components/button/NeuButton.vue
+++ src/components/button/NeuButton.vue
@@ -20,4 +20,17 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="css">
+button {
+  border-radius: 1.5em;
+  border: 0;
+  padding: 0.8em 2em;
+  background: #f1f3f8;
+  box-shadow: 10px 10px 20px #e0e2e5, -10px -10px 20px #fff;
+}
+
+button:hover {
+  box-shadow: 11px 11px 22px #e0e2e5, -10px -10px 20px #fff;
+  cursor: pointer;
+}
+</style>
