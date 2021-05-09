a4d532d809dd4769e1a01c1b853ca04f519dcba0
Phil Crossland <phil@thirdwheelstudios.com>
2021-05-09 22:06:12 +0100

    Initial NeuButton

---
+++ src/components/button/NeuButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button :disabled="disabled">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "neu-button",
+  props: {
+    disabled: {
+      type: Boolean,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
