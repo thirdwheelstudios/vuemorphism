fcdf3fa03b5f02077f3b2606a73dcd45a581444c
Phil Crossland <phil@thirdwheelstudios.com>
2021-05-09 22:08:17 +0100

    Deleted HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <neu-button>Test Button</neu-button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import NeuButton from "./components/button/NeuButton.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    NeuButton,
+  },
+  methods: {},
+});
 </script>
+NeuButton
 
 <style>
 #app {
